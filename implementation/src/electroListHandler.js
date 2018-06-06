91cff85149601137c84e7438deb460aaa37bbf46
DESKTOP-ENKK7LO\Roman <r.groiss18@gmail.com>
2018-06-06 16:56:11 +0200

    created function for electroListHandler to show ObjectNames in List.

--- implementation/src/electroListHandler.js
+++ implementation/src/electroListHandler.js
@@ -1,11 +1,25 @@
 var electroListHandler = {
-    buildList : function(lsItems) {
-        console.log(lsItems);
-    }
-    , clearList : function() {
+    buildList: function (lsItems) {
+ 
+        $.each(lsItems, function (index, value) {
+            var floorName = value['name'];
+            $("#floorName").prepend("<a href='#' class='list-group-item list-group-item-action'>" + floorName + "</a>");
+        });
+
+
+        
+    
 
     }
-    , bindEditOptions : function(){
+    , clearList: function () {
+        //löscht die aktuelle Liste
+
+    }
+    , bindEditOptions: function () {
+
+        //click eventlistener für die buttons - testen mit console.log
+
+        //CHECK BUTTONS FÜR FRONTEND
 
     }
 }
