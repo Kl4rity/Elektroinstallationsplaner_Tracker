2c6294c1edcea3d791a493ad8a08cd6be42cb33d
Clemens Stift <clemens@stift.online>
2018-06-06 14:57:35 +0200

    Created dummy data and function call to ListViewHandler for testing purposes.

---
+++ implementation/src/electroController.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    
+    lsDummyData = [{
+        name : "Erdgeschoss"
+        , floor_count_from_basement : 1
+        , id : 3
+        , parent_id : 4
+    },
+    {
+        name : "Erster Stock"
+        , floor_count_from_basement : 2
+        , id : 4
+        , parent_id : 4
+    },
+    {
+        name : "Zweiter Stock"
+        , floor_count_from_basement : 3
+        , id : 5
+        , parent_id : 4
+    },
+    {
+        name : "Keller"
+        , floor_count_from_basement : 0
+        , id : 6
+        , parent_id : 4
+    }];
+
+    electroListHandler.buildList(lsDummyData);
+
+});
