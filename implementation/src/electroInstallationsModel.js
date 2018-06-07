e1c2567c288be6c980cffa5ec2fed87983365123
Clemens Stift <clemens@stift.online>
2018-06-07 17:12:49 +0200

    Object creation logic complete. Function checking for currentListtype, routing list to correct object to be created.

--- implementation/src/electroInstallationsModel.js
+++ implementation/src/electroInstallationsModel.js
@@ -1,97 +1,143 @@
+var createElektroInstallationsItems = function (responseItem){
+        lsItems = [];
+        switch(responseItem.currentLevel){
+            case "projects":
+                rawItemList.forEach(function(listItem){
+                    lsItems.push(new Project(listItem, responseItem.currentLevel, responseItem.nextLevel)); 
+                });
+                break;
+            case "floors":
+                rawItemList.forEach(function(listItem){
+                    lsItems.push(new Floor(listItem, responseItem.currentLevel, responseItem.nextLevel)); 
+                });
+                break;
+
+            case "rooms":
+                rawItemList.forEach(function(listItem){
+                    lsItems.push(new Room(listItem, responseItem.currentLevel, responseItem.nextLevel)); 
+                });
+                break;
+
+            case "loaders":
+                rawItemList.forEach(function(listItem){
+                    lsItems.push(new Loader(listItem, responseItem.currentLevel, responseItem.nextLevel)); 
+                });
+                break;
+
+            case "sensors":
+                rawItemList.forEach(function(listItem){
+                    lsItems.push(new Sensor(listItem, responseItem.currentLevel, responseItem.nextLevel)); 
+                });
+                break;
+                
+            default:
+                console.log("ERROR: Listtype not known.");
+                return;
+        }
+        return lsItems;
+    }
+
 class electroInstallationItem {
-    constructor(name, id, created, lastChanged){
-        this.name = name;
-        this.id = id;
-        this.created = created;
-        this.lastChanged = lastChanged;
-        
-        this.deleteItem = function() {
-            console.log("AJAX request to delete the item will be triggered here.");
+    constructor(itemData, currentLevel, nextLevel){
+        this.name = itemData.name;
+        this.id = itemData.id;
+        this.created = itemData.created;
+        this.lastChanged = itemData.last_change;
+
+        this.fetchChildrenPostRequest = {
+            action : "getlist"
+            , listtype : nextLevel
+            , parentid : this.id
         }
 
-        this.fetchChildren = function() {
-            console.log("AJAX request to fetch Children will be triggered here.");
+        this.deletePostRequest = {
+            action : "delete"
+            , listtype : currentLevel
+            , itemid : this.id
+        };
+
+        this.updatePostRequest = {
+            action : "update"
+            , listtype : currentLevel
+            , itemid : this.id
+            , specification : {
+                
+            }
+        };
+        
+        this.ajaxRequest = function(postRequest) {
+            console.log("The post request is: " + postRequest);
+            $.post(window.location.orign, JSON.stringify(postRequest), alert(data));
+        }
+
+        this.fetchChildren = function(){
+            console.log("AJAX Request to fetch Children will be triggered here.");
+            this.ajaxRequest(this.fetchChildrenPostRequest);
+        }
+
+        this.update = function(specification){
+            console.log("AJAX request to update the item will be triggered here.");
+            console.log("Specification to be set: " + specification);
+            this.updatePostRequest.specification = specification;
+            this.ajaxRequest(this.updatePostRequest);
+        }
+
+        this.delete = function(){
+            console.log("AJAX request to delete item will be triggered here.");
+            this.ajaxRequest(this.deletePostRequest);
         }
     }
 }
 
 class Project extends electroInstallationItem {
-    constructor(responseItem){
-        super(responseItem.name, responseItem.id, responseItem.created, responseItem.last_change);
-
-        this.update = function(){
-            console.log("AJAX request to update the item will be triggered here.");
-        }
+    constructor(itemData, currentLevel, nextLevel){
+        super(itemData, currentLevel, nextLevel);
     }
 }
 
 class Floor extends electroInstallationItem {
-    constructor(responseItem){
-        super(responseItem.name, responseItem.id, responseItem.created, responseItem.last_change);
+    constructor(itemData, currentLevel, nextLevel){
+        super(itemData, currentLevel, nextLevel);
 
-        this.floorCountFromBasement = responseItem.floor_count_from_basement;
-        this.parentID = responseItem.projects_id;
-
-        this.update = function(){
-            console.log("AJAX request to update the item will be triggered here.");
-        }
+        this.floorCountFromBasement = itemData.floor_count_from_basement;
+        this.parentId = itemData.projects_id;
     }
 }
 
 class Room extends electroInstallationItem {
-    constructor(responseItem){
-        super(responseItem.name, responseItem.id, responseItem.created, responseItem.last_change);
+    constructor(itemData, currentLevel, nextLevel){
+        super(itemData, currentLevel, nextLevel);
 
-        this.parentID = responseItem.floors_id;
-
-        this.update = function(){
-            console.log("AJAX request to update the item will be triggered here.");
-        }
+        this.parentId = itemData.floors_id;
     }
 }
 
 class Loader extends electroInstallationItem {
-    constructor(responseItem){
-        super(responseItem.name, responseItem.id, responseItem.created, responseItem.last_change);
+    constructor(itemData, currentLevel, nextLevel){
+        super(itemData, currentLevel, nextLevel);
 
-        this.parentID = responseItem.rooms_id;
-
-        this.update = function(){
-            console.log("AJAX request to update the item will be triggered here.");
-        }
+        this.parentId = itemData.rooms_id;
     }
 }
 
 class Sensor extends electroInstallationItem {
-    constructor(responseItem){
-        super(responseItem.name, responseItem.id, responseItem.created, responseItem.last_change);
+    constructor(itemData, currentLevel, nextLevel){
+        super(itemData, currentLevel, nextLevel);
 
-        this.parentID = responseItem.devices_id;
-        this.unit = responseItem.unit;
-        this.value = responseItem.value;
-
-        this.update = function(){
-            console.log("AJAX request to update the item will be triggered here.");
-        }
+        this.parentId = itemData.devices_id;
+        this.unit = itemData.unit;
+        this.value = itemData.value;
     }
 }
 
 // class CircuitBreaker extends electroInstallationItem {
-//     constructor(responseItem){
-//         super(responseItem.name, responseItem.id, responseItem.created, responseItem.last_change);
-
-//         this.update = function(){
-//             console.log("AJAX request to update the item will be triggered here.");
-//         }
+//     constructor(itemData, currentLevel, nextLevel){
+//         super(itemData, currentLevel, nextLevel);
 //     }
 // }
 
 // class Fuse extends electroInstallationItem {
-//     constructor(responseItem){
-//         super(responseItem.name, responseItem.id, responseItem.created, responseItem.last_change);
-
-//         this.update = function(){
-//             console.log("AJAX request to update the item will be triggered here.");
-//         }
+//     constructor(itemData, currentLevel, nextLevel){
+//         super(itemData, currentLevel, nextLevel);
 //     }
 // }
