e5351a3ca599aa4784867329e018ef0b83524441
DESKTOP-ENKK7LO\Roman <r.groiss18@gmail.com>
2018-06-12 17:49:54 +0200

    added event listeners delete and edit to buttons, changed item view to table.

--- implementation/src/electroListHandler.js
+++ implementation/src/electroListHandler.js
@@ -1,25 +1,36 @@
 var electroListHandler = {
     buildList: function (lsItems) {
- 
+
+        $(".deleteButton").unbind();
+
         $.each(lsItems, function (index, value) {
             var floorName = value['name'];
-            $("#floorName").prepend("<a href='#' class='list-group-item list-group-item-action'>" + floorName + "</a>");
+            var listItem = "<tr> <td class='name'>" + floorName + "</td><td class='extraData'></td><td align='right'><div class='btn-group mr-2' role='group' aria-label='First group'>"
+                + "<button type ='button' class='btn btn-secondary editButton'><img src='../icons/edit.png' width='20px' height='20px'></button>"
+                + "<button type ='button' class='btn btn-secondary detailsButton'>S</button>"
+                + "<button type ='button' class='btn btn-secondary deleteButton'><img src='../icons/delete.png' width='20px' height='20px'></button>"
+                + "</div></td></tr> ";
+            $(".appendedRow").prepend(listItem);
         });
 
-
-        
-    
-
+        $(".deleteButton").click(this.deleteRow);
+        $(".editButton").click(this.editRow);
     }
-    , clearList: function () {
+
+    , deleteRow: function () {
         //löscht die aktuelle Liste
-
+        $(this).parents("tr:first")[0].remove();
     }
-    , bindEditOptions: function () {
 
+    , editRow: function () {
+        console.log("Clicked");
+        //$(this).parent().parent().siblings().html("");
+        //$(this).parent().parent().siblings().append("<td><input type='text' style='width: 100 % ' class='editName'></td> <td><input type='text' style='width:100%' class='editExtraData'></td>");
         //click eventlistener für die buttons - testen mit console.log
 
         //CHECK BUTTONS FÜR FRONTEND
 
     }
 }
+
+//funktionen der Objektinstanz auf die Buttons setzen und in der Console ausgeben
