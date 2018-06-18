7c6b2bb05def56cb5bbebd9b5259cca5bccec9fc
DESKTOP-ENKK7LO\Roman <r.groiss18@gmail.com>
2018-06-18 13:51:55 +0200

    amended eventlisteners for option buttons(within foreach)

--- implementation/src/electroListHandler.js
+++ implementation/src/electroListHandler.js
@@ -4,19 +4,31 @@ var electroListHandler = {
         $(".deleteButton").unbind();
 
         $.each(lsItems, function (index, value) {
-            var floorName = value['name'];
-            var listItem = "<tr> <td class='name'>" + floorName + "</td><td class='extraData'></td><td align='right'><div class='btn-group mr-2' role='group' aria-label='First group'>"
-                + "<button type ='button' class='btn btn-secondary editButton'><img src='../icons/edit.png' width='25px' height='25px'></button>"
+            var floorName = value.name;
+            var deleteButtonId = "deleteButton-" + index;
+            var editButtonId = "editButton-" + index;
+            var floorNameId = "floorName-" + index;
+
+            var listItem = "<tr> <td class='name' id=" + floorNameId + ">" + floorName + "</td><td class='extraData'></td><td align='right'><div class='btn-group mr-2' role='group' aria-label='First group'>"
+                + "<button type ='button' class='btn btn-secondary editButton' id=" + editButtonId + "><img src='../icons/edit.png' width='25px' height='25px'></button>"
                 + "<button type ='button' class='btn btn-secondary detailsButton'><img src='../icons/show.png' width='25px' height='25px'></button>"
-                + "<button type ='button' class='btn btn-secondary deleteButton'><img src='../icons/delete.png' width='25px' height='25px'></button>"
+                + "<button type ='button' class='btn btn-secondary deleteButton' id=" + deleteButtonId + "><img src='../icons/delete.png' width='25px' height='25px'></button>"
                 + "</div></td></tr> ";
             $(".appendedRow").prepend(listItem);
+            $("#" + deleteButtonId).click(value.delete);
+            $("#" + editButtonId).click(value.update);
+            $("#" + floorNameId).click(value.fetchChildren);
+            $("#" + floorNameId).hover(this.hoverEffect);
+
+
         });
 
-        $(".deleteButton").click(this.deleteRow);
-        $(".detailsButton").click(this.showDetails);
-        $(".editButton").click(this.editRow);
-        $(".saveBtn").click(this.createItem);
+
+
+        //$(".deleteButton").click(this.delete);
+        //$(".detailsButton").click(this.showDetails);
+        //$(".editButton").click(this.editRow);
+        //$(".saveBtn").click(this.createItem);
     }
 
     , deleteRow: function () {
@@ -28,7 +40,7 @@ var electroListHandler = {
         var currentLevel = 1;
         var nextLevel = 2;
         var deleteItem = new electroInstallationItem(itemData, currentLevel, nextLevel);
-        deleteItem.delete();
+        electroInstallationItem.delete();
     }
 
     , showDetails: function () {
@@ -36,11 +48,7 @@ var electroListHandler = {
     }
 
     , editRow: function () {
-        var itemData = 5;
-        var currentLevel = 1;
-        var nextLevel = 2;
-        var updateItem = new electroInstallationItem(itemData, currentLevel, nextLevel);
-        updateItem.update();
+        console.log("Seas!");
 
     }
 
@@ -51,6 +59,14 @@ var electroListHandler = {
         var newItem = new electroInstallationItem(itemData, currentLevel, nextLevel);
         newItem.create();
     }
+
+    , hoverEffect: function (floorNameId) {
+            $(this).css("background-color", "yellow");
+            $(this).css("text-decoration", "underline");
+        }, function () {
+            $(this).css("background-color", "");
+            $(this).css("text-decoration", "none");
+        }
 }
 
 //funktionen der Objektinstanz auf die Buttons setzen und in der Console ausgeben
