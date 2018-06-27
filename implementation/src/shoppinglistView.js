35e6c8563df47ccb6041d31ebb52cff30bf6c797
DESKTOP-ENKK7LO\Roman <r.groiss18@gmail.com>
2018-06-27 16:37:19 +0200

    created shoppingListView and functionality to load existing projects to shoppingListView

--- implementation/src/shoppinglistView.js
+++ implementation/src/shoppinglistView.js
@@ -1,5 +1,10 @@
 var shoppinglistView = {
     getProjects: function (lsItems) {
-        console.log(lsItems);
+        $.each(lsItems, function (index, value) {
+            console.log(value);
+            var projectName = value.name;
+            var projectNameItem = "<tr itemId=" + value.id + "> <td>" + projectName + "</td></tr>";
+            $(".shoppingList").prepend(projectNameItem);
+        })
     }
 }
