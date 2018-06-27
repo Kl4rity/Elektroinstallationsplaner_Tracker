4e05e323b919973f43d52e324930313055851df7
DESKTOP-ENKK7LO\Roman <r.groiss18@gmail.com>
2018-06-27 12:01:58 +0200

    finished implementation to update project title

--- implementation/src/electroListHandler.js
+++ implementation/src/electroListHandler.js
@@ -4,14 +4,14 @@ var electroListHandler = {
         $(".appendedRow").empty();
         
         $.each(lsItems, function (index, value) {
- 
+
             objectName = value.constructor.name.toUpperCase();
             floorName = value.name;
             var deleteButtonId = "deleteButton-" + index;
             var editButtonId = "editButton-" + index;
             var floorNameId = "floorName-" + index;
 
-            var listItem = "<tr itemId=" + value.id +"> <td class='name' id=" + floorNameId + ">" + floorName + "</td><td class='extraData'></td><td align='right'><div class='btn-group mr-2' role='group' aria-label='First group'>"
+            var listItem = "<tr itemId=" + value.id + "> <td class='name' id=" + floorNameId + ">" + floorName + "</td><td class='extraData'></td><td align='right'><div class='btn-group mr-2' role='group' aria-label='First group'>"
                 + "<button type ='button' class='btn btn-secondary editButton' id=" + editButtonId + " data-toggle='modal' data-target='#exampleModalCenter'><img src='../icons/edit.png' width='25px' height='25px'></button>"
                 + "<button type ='button' class='btn btn-secondary detailsButton'><img src='../icons/show.png' width='25px' height='25px'></button>"
                 + "<button type ='button' class='btn btn-secondary deleteButton' id=" + deleteButtonId + "><img src='../icons/delete.png' width='25px' height='25px'></button>"
@@ -19,18 +19,20 @@ var electroListHandler = {
             $(".appendedRow").prepend(listItem);
 
             $("#" + deleteButtonId).click(value.delete);
-            
-            $("#" + editButtonId).click(function(){
+
+            $("#" + editButtonId).click(function () {
                 addDialogueView.editExistingEntryDialogue($(this));
             });
             $("#" + floorNameId).click(value.fetchChildren);
 
-            
-            
+            //Change Title of Project when ProjectName is clicked
+            $(".name").click(function () {
+                if (objectName == "PROJECT") {
+                    var projectTitle = $(this).text();
+                    $("#projectHeadTitle").text(projectTitle);
+                }
+            });           
         });
-
-        this.getProjectName();
-
         this.showStageTitle(); 
     }
 
@@ -64,26 +66,8 @@ var electroListHandler = {
         $("#listTitle").text(objectName + "S");
     }
 
-    , getProjectName: () => {  
-        var newObjectName;
-        var setNewObjectName =  () => {
-            if (objectName == "PROJECT") {
-                console.log(objectName);
-                $(".name").click(function () {
-                    newObjectName = $(this).text();
-                    return newObjectName;
-                })  
-                
-            }   
-            
-        }
-        console.log(newObjectName);
+    , createProjectTitle: function () {
+        var projectTitle = $(this).text();
+        console.log(projectTitle);
     }
-
-    , updateProjectName: function () {
-        var newName = this.getProjectName();
-        $("#projectHeadTitle").text(newName);
-        }
 }
-
-//funktionen der Objektinstanz auf die Buttons setzen und in der Console ausgeben
