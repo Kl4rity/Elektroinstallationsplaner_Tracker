ed8d4e8f05e7420492a8ad1a36ee4e1c738a6ab1
Clemens Stift <clemens@stift.online>
2018-07-03 15:19:01 +0200

    Greatly reduced amount of code in electroController

--- implementation/src/electroController.js
+++ implementation/src/electroController.js
@@ -1,63 +1,15 @@
 var ElectroController = {
-    currentBackendAddress : "http://localhost/Semester2Project/Elektroinstallationsplaner_Tracker/implementation/backend/index.php"
-    , requestHistory : []
-    , fetchData : function(requestObject){
-        if(!Object.is(requestObject, ElectroController.requestHistory[ElectroController.requestHistory.length -1])){
-            ElectroController.requestHistory.push(requestObject);
-        }
-
-        $.ajax({
-            url: ElectroController.currentBackendAddress,
-            type: "post",
-            data: {data: JSON.stringify(requestObject)},
-            dataType: "json",
-            cache: false,
-            success: function(data){
-                switchView("page1");
-                console.log(data);
-                currentLevelElektroinstallationsItems = createElektroInstallationsItems(data);
-                electroListHandler.buildList(currentLevelElektroinstallationsItems, requestObject.listtype, requestObject.parentid);
-                addDialogueView.initDialogue(data.currentLevel.toLowerCase(), currentLevelElektroinstallationsItems);
-                sidebarView.highlightStage(currentLevelElektroinstallationsItems);
-                shoppinglistView.getProjects(currentLevelElektroinstallationsItems);
-            },
-            error: function(data){
-                console.log("ERROR\n" + data);
-            }
-        });
-    }
-    , reloadCurrentData : function(){
-        ElectroController.fetchData(ElectroController.requestHistory[ElectroController.requestHistory.length -1]);
-    }
-    , createNewElement : function(requestObject){
-        $.ajax({
-            url: ElectroController.currentBackendAddress,
-            type: "post",
-            data: {data: JSON.stringify(requestObject)},
-            dataType: "json",
-            cache: false,
-            success: function(){
-                ElectroController.reloadCurrentData();
-            },
-            error: function(data){
-                console.log("ERROR\n" + data);
-            }
-        });
-    }
+    startPage : {action : "getlist" , listtype : "projects" , parentid : "0"}
     , onBackButton : function(){
-        if(ElectroController.requestHistory.length > 1){
-            ElectroController.requestHistory.pop();
-            ElectroController.reloadCurrentData();
-        } else {
-            ElectroController.reloadCurrentData();
-        }
+        queryService.goUpOneLevel();
+    }
+    , onLogo : function(){
+        queryService.goUpToLevel({nameOfList: "projects"});
     }
 }
 
 $(document).ready(function(){
-
-    var initialRequest = {action : "getlist" , listtype : "PROJECTS" , parentid : "1"};
-
-    $("#backButton").click(ElectroController.onBackButton); 
-    ElectroController.fetchData(initialRequest);
+    $("#backButton").click(ElectroController.onBackButton);
+    $("#main-logo").click(ElectroController.onLogo);
+    queryService.loadNewView(ElectroController.startPage);
 });
