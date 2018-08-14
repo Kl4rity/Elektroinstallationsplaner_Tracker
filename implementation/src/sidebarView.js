71091a796c281d6aaac7167d93762713f5f92924
Clemens Stift <clemens@stift.online>
2018-08-14 16:47:49 +0200

    Implemented logic knowing when Circuitry becomes available and passing the project ID to it.

--- implementation/src/sidebarView.js
+++ implementation/src/sidebarView.js
@@ -1,25 +1,55 @@
 var sidebarView = {
-    highlightStage: function (lsItems) {
-        
+    projectId : null
 
+    , highlightStage: function (lsItems) {
         $.each(lsItems, function (index, value) {
             stageName = value.constructor.name;
-            //console.log(stageName);
-
+            console.log(stageName);
         })
-
         this.chooseStage(stageName);
-        
     }
 
     , chooseStage: function (stageName) {
-        var stages = ["Floor", "Room", "Loader", "Sensor"];
+        var stages = ["Floor", "Room", "Loader", "Sensor", "CircuitBreaker", "Fuse", "Wiring"];
         for (i = 0; i < stages.length; i++) {
             if (stageName == stages[i]) {
-                $("." + stageName).css({ "background": "white", "color": "rgb(124, 198, 214)" });
+                $("." + stageName).addClass("currentlyActiveElement");
             } else {
-                $("." + stages[i]).css({ "background": "rgb(124, 198, 214)", "color": "white" });
+                $("." + stages[i]).removeClass("currentlyActiveElement");
+            }
+        }   
     }
-}}
 
+    , handleChangeOfView : function(requestObject){
+        if (requestObject.listtype == "floors"){
+            sidebarView.projectId = requestObject.parentid;
+            sidebarView.attachLinkToCircuitBreakerHook();
+        }
+        if (requestObject.listtype == "projects"){
+            sidebarView.projectId = null;
+            sidebarView.removeLinkFromCircuitBreakerHook();
+        }
+    }
+
+    , checkWhetherDevicesExist : function(){
+
+    }
+
+    , checkWhetherFusesExist : function(){
+
+    }
+
+    , isReadyForWiring : function(){
+
+    }
+    , attachLinkToCircuitBreakerHook : function(){
+        $("#circuitBreakerHook").on("click", function(){
+            queryService.loadNewView({action : "getlist" , listtype : "CIRCUIT_BREAKERS" , parentid : sidebarView.projectId});
+        });
+        $("#circuitBreakerHook").addClass("activeSidebarLink");
+    }
+    , removeLinkFromCircuitBreakerHook : function(){
+        $("#circuitBreakerHook").off("click");
+        $("#circuitBreakerHook").removeClass("activeSidebarLink");
+    }
 }
