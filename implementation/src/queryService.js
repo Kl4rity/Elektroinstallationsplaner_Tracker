7370d0e185206669c6b223347875b121daedcef8
Clemens Stift <clemens@stift.online>
2018-08-14 16:49:10 +0200

    Added a passthrough of information to SidebarView (current request) for sidebarView to be able to react to changes.

--- implementation/src/queryService.js
+++ implementation/src/queryService.js
@@ -2,6 +2,11 @@ var queryService = {
     currentBackendAddress : "http://localhost/Semester2Project/Elektroinstallationsplaner_Tracker/implementation/backend/index.php"
     , queryHistory : []
     , loadNewView : function(requestObject){
+        console.log(requestObject);
+
+        //pass Data to SidebarView
+        sidebarView.handleChangeOfView(requestObject);
+
         if(!Object.is(requestObject, queryService.queryHistory[queryService.queryHistory.length -1])){
             queryService.queryHistory.push(requestObject);
         }
@@ -28,6 +33,7 @@ var queryService = {
         queryService.loadNewView(queryService.queryHistory[queryService.queryHistory.length -1]);
     }
     , createUpdateDeleteQuery : function(requestObject){
+        console.log(requestObject);
         $.ajax({
             url: queryService.currentBackendAddress,
             type: "post",
