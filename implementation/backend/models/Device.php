3bdd88512a3fd744d1d2a61403d5558902a1044c
Clemens Stift <clemens@stift.online>
2018-08-14 14:28:50 +0200

    Allow for a device to be created without a fuse_id. Simple if-statement to check whether fuse_id is set or not.

--- implementation/backend/models/Device.php
+++ implementation/backend/models/Device.php
@@ -14,13 +14,23 @@
 class Device extends ElectricModelItem {
     //put your code here
     public function create($parentId, $specification) {
+        // If fuse_id is not set, execute query without it.
+        if (empty($specification->fuse_id)) {
+            $sql = " INSERT INTO " . $this->tableNameString . "(".$this->parentIdTypeString.", name) VALUES (". $parentId . " , '" . $specification->name . "')";
+            return $this->database->query($sql);
+        }
+
         $sql = " INSERT INTO " . $this->tableNameString . "(".$this->parentIdTypeString.", name, fuses_id) VALUES (". $parentId . " , '" . $specification->name . "' " . $specification->fuses_id . ")";
         return $this->database->query($sql);
     }
 
     public function update($itemId, $specification) {
-        $sql = " UPDATE ". $this->tableNameString . " SET name = '" . $specification->name . "' WHERE id = " . $itemId . "";
+        if (empty($specification->fuse_id)) {
+            $sql = " UPDATE ". $this->tableNameString . " SET name = '" . $specification->name . "' WHERE id = " . $itemId . "";
+            return $this->database->query($sql);
+        }
+
+        $sql = " UPDATE ". $this->tableNameString . " SET name = '" . $specification->name . "', fuses_id = '" . $specification->fuse_id . "' WHERE id = " . $itemId . "";
         return $this->database->query($sql);
     }
-
 }
